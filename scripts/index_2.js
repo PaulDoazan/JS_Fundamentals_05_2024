8842830a5bfe996fc67b6d6f6230af7b2dbeade6
pdoazan <paul.doazan@gmail.com>
2024-05-14 11:01:49 +0200

    Add array gender

--- scripts/index_2.js
+++ scripts/index_2.js
@@ -1,1 +1,20 @@
-console.log("Hello day 2")
+const limit = 50
+
+const arrNames = ["Simon", "Stéphanie", "Jamie", "Hannah", "Stéphanie", "Jamie", "Hannah"]
+const people = [
+    { name: "Simon", gender: "male" },
+    { name: "Paul", gender: "male" },
+    { name: "Mathilde", gender: "female" },
+    { name: "Marianne", gender: "female" },
+    { name: "Louis", gender: "male" },
+]
+
+for (let i = 0; i < arrNames.length; i++) {
+    console.log(arrNames[i])
+}
+
+// console.log(arrNames[0])
+// console.log(arrNames[1])
+// console.log(arrNames[2])
+// console.log(arrNames[3])
+// console.log(arrNames[4])
