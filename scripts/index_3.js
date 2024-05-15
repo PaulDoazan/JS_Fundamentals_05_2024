e9b5148cf45a85e7e5869b3719590986ac5da9e1
pdoazan <paul.doazan@gmail.com>
2024-05-15 10:15:41 +0200

    Add passwords array

---
+++ scripts/index_3.js
@@ -0,0 +1,32 @@
+console.log("Start day 3")
+
+for (let i = 0; i <= 20; i += 3) {
+    console.log(i * 10)
+}
+
+let passwords = [
+    "password1",
+    "qwerty123",
+    "dragon",
+    "1234",
+    "baseball",
+    "iloveyou",
+    "trustno1",
+    "sunshine",
+    "princess",
+    "football",
+    "welcome",
+    "shadow",
+    "superman",
+    "michael",
+    "ninja",
+    "mustang",
+    "jessica",
+    "charlie",
+    "ashley",
+    "bailey",
+    "passw0rd",
+    "master"
+]
+
+// Afficher dans la console tous les mots de passe, avec une boucle for
