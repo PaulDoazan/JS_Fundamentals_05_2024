e18d39220610740291083d73b3ce7d9b49d3afac
pdoazan <paul.doazan@gmail.com>
2024-05-16 10:03:19 +0200

    Add file 4

---
+++ scripts/index_4.js
@@ -0,0 +1,25 @@
+console.log('dans le fichier 4')
+
+const pers1 = {
+    firstname: "Antoine",
+    lastname: "Dupont",
+    age: 30,
+    town: "Toulouse"
+}
+
+const pers2 = {
+    firstname: "Stéphane",
+    lastname: "Durand",
+    age: 40,
+    town: "Nantes"
+}
+
+let helloSentence = sayHello()
+helloSentence += " ?"
+console.log(helloSentence)
+// Bonjour Stéphane Durand, fait-il beau à Nantes ?
+
+
+function sayHello() {
+
+}
