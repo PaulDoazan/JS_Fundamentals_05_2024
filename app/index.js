768b26bbc035a942ca5262cdf0d628ce9cb13c4f
pdoazan <paul.doazan@gmail.com>
2024-05-16 12:56:42 +0200

    WIP js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
 console.log('ma web-app')
+
+// querySelector -> button
+// querySelector -> body
+
+const btn = document.querySelector(".btn-change")
+const body = document.querySelector("body")
+
+btn.addEventListener("click", changeColor)
+
+function changeColor() {
+    console.log("click")
+    body.style.backgroundColor = `rgb(0,0,0)`
+    // on change de façon aléatoire la couleur du body
+}
