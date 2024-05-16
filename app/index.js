d132eb6e286fb6d444a62db9363e9cfd9b3459f0
pdoazan <paul.doazan@gmail.com>
2024-05-16 13:08:09 +0200

    Wip change color on btn click

--- app/index.js
+++ app/index.js
@@ -1,8 +1,5 @@
 console.log('ma web-app')
 
-// querySelector -> button
-// querySelector -> body
-
 const btn = document.querySelector(".btn-change")
 const body = document.querySelector("body")
 
@@ -10,6 +7,14 @@ btn.addEventListener("click", changeColor)
 
 function changeColor() {
     console.log("click")
-    body.style.backgroundColor = `rgb(0,0,0)`
-    // on change de façon aléatoire la couleur du body
+    body.style.backgroundColor = getRandomColor()
+    // on change de façon aléatoire la couleur du body, Math.random()
+}
+
+function getRandomColor() {
+    const red = Math.round(Math.random() * 255)
+    const green = Math.round(Math.random() * 255)
+    const blue = Math.round(Math.random() * 255)
+
+    return `rgb(${red}, ${green}, ${blue})`
 }
