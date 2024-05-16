55a71baaf0315694070bd099ee6d5a861e6a7be0
pdoazan <paul.doazan@gmail.com>
2024-05-16 11:15:37 +0200

    Correct bmi round

--- scripts/index_4.js
+++ scripts/index_4.js
@@ -50,11 +50,16 @@ function sayHello(obj) {
 // écrire une fonction qui prend en paramètre une personne et qui retourne son IMC
 // imc =>  poids / taille²
 
-const bmiPers1 = bmi(pers1)
-console.log(bmiPers1)
+for (let i = 0; i < people.length; i++) {
+    const bmiPers = bmi(people[i])
+    console.log(bmiPers)
+}
+
 
 // Implémenter la boucle for() pour calculer toutes les IMC
+// Arrondir à 2 chiffres après la virgule le résultat, Math.round()
 function bmi(obj) {
     const result = obj.weight / Math.pow(obj.size, 2)
-    return result
+    // return Math.round(result * 100) / 100
+    return result.toFixed(2)
 }
